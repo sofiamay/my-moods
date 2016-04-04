7c4074e1bf608037c2d57e634a030e47a90f1897
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-04 16:47:05 -0700

    Separate user into its own template

--- imports/ui/user.js
+++ imports/ui/user.js
@@ -1,10 +1,10 @@
 import { Template } from 'meteor/templating';
 import { Moods } from '../api/moods.js';
  
- import './mood.js'
+import './mood.js'
 import './user.html';
  
-Template.body.helpers({
+Template.user.helpers({
   moods() {
     return Moods.find({}, { sort: { createdAt: -1 } });
   },
