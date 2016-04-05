d685dd02ac9aa7a25f6a67b79e8408f8a062bc01
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-05 00:24:34 -0700

    Allow user to submit a new Mood to the database

--- imports/ui/create-mood.js
+++ imports/ui/create-mood.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Behaviors } from '../api/behaviors.js';
+import { Moods } from '../api/moods.js';
   
 import './behavior.js'
 import './create-mood.html';
@@ -15,7 +16,26 @@ Template.createMood.events({
     var target = instance.$('#newBehavior');
     var text = target.val();
 
+    //make sure to add the user once auth is implemented!
     Behaviors.insert({name: text, value: true});
     target.val('');
+  },
+  'submit .new-mood'(event){
+    event.preventDefault();
+    const $form = ($(event.target));
+    const polarity = $form.find('.polarity').val();
+    const description = $form.find('.textarea').val();
+    const checkedBehaviors = $form.find("input:checked");
+    var behaviors = [];
+    for (var i = 0; i<checkedBehaviors.length; i++) {
+      behaviors.push(checkedBehaviors[i].value);
+    }
+
+    Moods.insert({
+          description: description,
+          polarity: polarity,
+          behaviors: behaviors,
+          createdAt: new Date(), // current time
+        });
   }
 });
