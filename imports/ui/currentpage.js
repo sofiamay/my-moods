72fcf52a687a4810c9305fba16a2d3f91eb798c5
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-05 10:26:13 -0700

    Touch up naviagation template

--- imports/ui/currentpage.js
+++ imports/ui/currentpage.js
@@ -1,7 +1,8 @@
 import { Template } from 'meteor/templating';
 
 //List of Templates available to dynamically display:
-import './login.js'
+// import './nav.js';
+import './login.js';
 import './create-mood.js';
 import './user.js'; 
 import './currentpage.html';
