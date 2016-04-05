6b3d1750612c81b5916046e4e7a722b59eb34fdf
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-05 15:10:46 -0700

    Add login form

--- imports/ui/login.js
+++ imports/ui/login.js
@@ -1,3 +1,21 @@
 import { Template } from 'meteor/templating';
 
 import './login.html';
+
+Template.login.onRendered(function(){
+  // Jquery functionality. Switch between login and signup forms
+  $('#login-form-link').click(function(e) {
+      $("#login-form").delay(100).fadeIn(100);
+      $("#register-form").fadeOut(100);
+      $('#register-form-link').removeClass('active');
+      $(this).addClass('active');
+      e.preventDefault();
+    });
+    $('#register-form-link').click(function(e) {
+      $("#register-form").delay(100).fadeIn(100);
+      $("#login-form").fadeOut(100);
+      $('#login-form-link').removeClass('active');
+      $(this).addClass('active');
+      e.preventDefault();
+    });
+});
