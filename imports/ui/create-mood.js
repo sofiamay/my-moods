4201e1eda98ae86a3b936aeb0517c00f13fe71a0
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-05 12:26:14 -0700

    Attach user Id to all posts to the database

--- imports/ui/create-mood.js
+++ imports/ui/create-mood.js
@@ -17,9 +17,12 @@ Template.createMood.events({
   'click #addBehavior'(event, instance) {
     var target = instance.$('#newBehavior');
     var text = target.val();
+    userId = Meteor.userId();
+    //no userId? Error, redirect to login
+    if (!userId) {FlowRouter.go('/login');}
 
     //make sure to add the user once auth is implemented!
-    Behaviors.insert({name: text, value: true});
+    Behaviors.insert({name: text, value: true, user: userId});
     target.val('');
   },
 
@@ -30,6 +33,9 @@ Template.createMood.events({
     const polarity = $form.find('.polarity').val();
     const description = $form.find('.textarea').val();
     const checkedBehaviors = $form.find("input:checked");
+    userId = Meteor.userId();
+    //no userId? Error, redirect to login
+    if (!userId) {FlowRouter.go('/login');}
     var behaviors = [];
     for (var i = 0; i<checkedBehaviors.length; i++) {
       behaviors.push(checkedBehaviors[i].value);
@@ -40,6 +46,7 @@ Template.createMood.events({
           description: description,
           polarity: polarity,
           behaviors: behaviors,
+          user: userId,
           createdAt: new Date(), // current time
         });
 
