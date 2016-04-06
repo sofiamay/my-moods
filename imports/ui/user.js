bfcb24af02a684edbc1c2a6d6d56c67eff7a1890
Sofia Berlin <sofiamberlin@gmail.com>
2016-04-05 17:45:06 -0700

    Add feature to search by behavior

--- imports/ui/user.js
+++ imports/ui/user.js
@@ -4,8 +4,33 @@ import { Moods } from '../api/moods.js';
 import './mood.js'
 import './user.html';
  
+// Template.user.helpers({
+//   moods() {
+//     return Moods.find({user: Meteor.userId()}, { sort: { createdAt: -1 } });
+//   },
+// });
+
 Template.user.helpers({
   moods() {
-    return Moods.find({user: Meteor.userId()}, { sort: { createdAt: -1 } });
-  },
+    // build a regular expression based on the current search
+    var search = Session.get('search');
+    var re = new RegExp(search, 'i');
+
+    selector = {user: Meteor.userId()};
+
+    // change this to be the right filter
+    if (search && search.length) {
+      selector.behaviors = re;
+  	}
+
+    options = {sort: {createdAt: -1}};
+    return Moods.find(selector, options);
+  }
+});
+
+Template.user.events({
+  'keyup #search': function() {
+    // save the current search query in a session variable as the user types
+    return Session.set('search', $('#search').val());
+  }
 });
